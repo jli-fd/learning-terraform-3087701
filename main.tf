426b92e975ecc9d56d58cc7643e8d92ad6db8447
jli-fd <jli1@firstderivatives.com>
2024-09-30 13:03:23 -0400

    fix group id ref

--- main.tf
+++ main.tf
@@ -75,7 +75,7 @@ module "blog_alb" {
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
-      target_id        = autoscaling_group_id
+      target_id        = module.autoscaling.autoscaling_group_id
     }
   }
 
